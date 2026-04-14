2b7ef14958042f06ae0342da7505650437e08024
Sam Reid <reids@colorado.edu>
2026-04-13 21:52:55 -0600

    sync-to-poly: Extract WaveVisualizationNode and WaveVisualizationCanvasNode into common/ with WaveVisualizableScene interface for shared wave rendering across High Intensity and Single Particles screens, see https://github.com/phetsims/turing/issues/7

--- js/common/view/WaveVisualizationNode.ts
+++ js/common/view/WaveVisualizationNode.ts
@@ -1,17 +1,19 @@
 // Copyright 2026, University of Colorado Boulder
 
 /**
- * WaveVisualizationNode is the large black rectangle representing the wave visualization region in the
- * High Intensity screen. It contains a canvas-based wave field rendering that updates each frame.
+ * WaveVisualizationNode is the black rectangle representing the wave visualization region. It contains
+ * a canvas-based wave field rendering that updates each frame. Used by both the High Intensity and
+ * Single Particles screens.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
 
+import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import Node, { NodeOptions } from '../../../../scenery/js/nodes/Node.js';
 import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
-import HighIntensityConstants from '../HighIntensityConstants.js';
-import HighIntensityModel from '../model/HighIntensityModel.js';
+import type { WaveVisualizableScene } from '../model/WaveVisualizableScene.js';
+import QuantumWaveInterferenceConstants from '../QuantumWaveInterferenceConstants.js';
 import WaveVisualizationCanvasNode from './WaveVisualizationCanvasNode.js';
 
 type SelfOptions = EmptySelfOptions;
@@ -24,19 +26,22 @@ export default class WaveVisualizationNode extends Node {
 
   private readonly waveCanvas: WaveVisualizationCanvasNode;
 
-  public constructor( model: HighIntensityModel, providedOptions?: WaveVisualizationNodeOptions ) {
+  public constructor( sceneProperty: TReadOnlyProperty<WaveVisualizableScene>, providedOptions?: WaveVisualizationNodeOptions ) {
 
     const options = optionize<WaveVisualizationNodeOptions, SelfOptions, NodeOptions>()( {}, providedOptions );
 
     super( options );
 
-    const backgroundRect = new Rectangle( 0, 0, HighIntensityConstants.WAVE_REGION_WIDTH, HighIntensityConstants.WAVE_REGION_HEIGHT, {
+    const width = QuantumWaveInterferenceConstants.WAVE_REGION_WIDTH;
+    const height = QuantumWaveInterferenceConstants.WAVE_REGION_HEIGHT;
+
+    const backgroundRect = new Rectangle( 0, 0, width, height, {
       cornerRadius: CORNER_RADIUS,
       fill: 'black'
     } );
     this.addChild( backgroundRect );
 
-    this.waveCanvas = new WaveVisualizationCanvasNode( model );
+    this.waveCanvas = new WaveVisualizationCanvasNode( sceneProperty, width, height );
     this.waveCanvas.clipArea = backgroundRect.getShape()!;
     this.addChild( this.waveCanvas );
   }
