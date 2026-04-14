ba508a899bf51bfa5bd42cf88a9410105b1c789e
Sam Reid <reids@colorado.edu>
2026-04-14 00:50:38 -0600

    sync-to-poly: Extract WavePlotChartNode and fix activeWaveDisplayModeProperty dependency bug in TimePlotNode and PositionPlotNode, see https://github.com/phetsims/turing/issues/7
    Mono-SHA: 4148ea37149f2a2e8737130e99b4d691a62f9238

---
+++ js/common/view/PositionPlotNode.ts
@@ -0,0 +1,208 @@
+// Copyright 2026, University of Colorado Boulder
+
+/**
+ * PositionPlotNode is a tool that shows the currently displayed wave quantity versus horizontal
+ * position along a draggable horizontal dotted line across the wave visualization region.
+ * It consists of a chart panel connected to a horizontal sampling line with a crosshair.
+ *
+ * Analogous to the WaveAreaGraphNode in wave-interference.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import Property from '../../../../axon/js/Property.js';
+import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
+import Bounds2 from '../../../../dot/js/Bounds2.js';
+import Range from '../../../../dot/js/Range.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
+import Vector2Property from '../../../../dot/js/Vector2Property.js';
+import Shape from '../../../../kite/js/Shape.js';
+import { clamp } from '../../../../dot/js/util/clamp.js';
+import WireNode from '../../../../scenery-phet/js/WireNode.js';
+import DragListener from '../../../../scenery/js/listeners/DragListener.js';
+import Circle from '../../../../scenery/js/nodes/Circle.js';
+import Line from '../../../../scenery/js/nodes/Line.js';
+import Node from '../../../../scenery/js/nodes/Node.js';
+import type { WaveVisualizableScene } from '../model/WaveVisualizableScene.js';
+import getDisplayedWaveValue from '../model/getDisplayedWaveValue.js';
+import QuantumWaveInterferenceColors from '../QuantumWaveInterferenceColors.js';
+import QuantumWaveInterferenceConstants from '../QuantumWaveInterferenceConstants.js';
+import QuantumWaveInterferenceFluent from '../../QuantumWaveInterferenceFluent.js';
+import WavePlotChartNode from './WavePlotChartNode.js';
+import waveDisplayModeYAxisLabelProperty from './waveDisplayModeYAxisLabelProperty.js';
+
+const CROSSHAIR_RADIUS = 12;
+const WIRE_LINE_WIDTH = 3;
+const WIRE_NORMAL_DISTANCE = 25;
+const DOTTED_LINE_DASH = [ 6, 4 ];
+const MIN_Y_FRACTION = 0.02;
+const MAX_Y_FRACTION = 0.98;
+
+export default class PositionPlotNode extends Node {
+
+  private readonly sceneProperty: TReadOnlyProperty<WaveVisualizableScene>;
+  private readonly chartNode: WavePlotChartNode;
+
+  // Normalized y-position of the horizontal line [0, 1] where 0 = top and 1 = bottom of the wave region
+  private readonly lineYFractionProperty: NumberProperty;
+
+  private readonly waveRegionX: number;
+  private readonly waveRegionY: number;
+
+  public constructor(
+    sceneProperty: TReadOnlyProperty<WaveVisualizableScene>,
+    waveRegionX: number,
+    waveRegionY: number,
+    visibleProperty: TReadOnlyProperty<boolean>
+  ) {
+    super( { visibleProperty: visibleProperty } );
+
+    this.sceneProperty = sceneProperty;
+    this.waveRegionX = waveRegionX;
+    this.waveRegionY = waveRegionY;
+
+    const waveRegionWidth = QuantumWaveInterferenceConstants.WAVE_REGION_WIDTH;
+    const waveRegionHeight = QuantumWaveInterferenceConstants.WAVE_REGION_HEIGHT;
+
+    this.lineYFractionProperty = new NumberProperty( 0.5, {
+      range: new Range( MIN_Y_FRACTION, MAX_Y_FRACTION )
+    } );
+
+    const dottedLine = new Line( waveRegionX, 0, waveRegionX + waveRegionWidth, 0, {
+      stroke: 'white',
+      lineWidth: 1.5,
+      lineDash: DOTTED_LINE_DASH,
+      opacity: 0.7
+    } );
+    this.addChild( dottedLine );
+
+    const crosshairNode = new Node( { cursor: 'ns-resize' } );
+    const crosshairBg = new Circle( CROSSHAIR_RADIUS, {
+      fill: 'rgba(0,0,0,0.2)',
+      stroke: 'white',
+      lineWidth: 2
+    } );
+    crosshairNode.addChild( crosshairBg );
+
+    const lineOptions = { stroke: 'white', lineWidth: 2 };
+    crosshairNode.addChild( new Line( -CROSSHAIR_RADIUS, 0, CROSSHAIR_RADIUS, 0, lineOptions ) );
+    crosshairNode.addChild( new Line( 0, -CROSSHAIR_RADIUS, 0, CROSSHAIR_RADIUS, lineOptions ) );
+
+    crosshairNode.x = waveRegionX + waveRegionWidth / 2;
+    this.addChild( crosshairNode );
+
+    const crosshairPositionProperty = new Vector2Property( new Vector2(
+      waveRegionX + waveRegionWidth / 2,
+      waveRegionY + waveRegionHeight * 0.5
+    ) );
+
+    const crosshairDragBounds = new Bounds2(
+      waveRegionX + waveRegionWidth / 2,
+      waveRegionY + waveRegionHeight * MIN_Y_FRACTION,
+      waveRegionX + waveRegionWidth / 2,
+      waveRegionY + waveRegionHeight * MAX_Y_FRACTION
+    );
+
+    crosshairNode.addInputListener( new DragListener( {
+      positionProperty: crosshairPositionProperty,
+      dragBoundsProperty: new Property( crosshairDragBounds )
+    } ) );
+
+    crosshairPositionProperty.link( position => {
+      const fraction = clamp( ( position.y - waveRegionY ) / waveRegionHeight, MIN_Y_FRACTION, MAX_Y_FRACTION );
+      this.lineYFractionProperty.value = fraction;
+    } );
+
+    this.lineYFractionProperty.link( fraction => {
+      const viewY = waveRegionY + fraction * waveRegionHeight;
+      dottedLine.y1 = viewY;
+      dottedLine.y2 = viewY;
+      crosshairNode.y = viewY;
+      crosshairPositionProperty.value = new Vector2( crosshairPositionProperty.value.x, viewY );
+    } );
+
+    const yAxisLabelStringProperty = waveDisplayModeYAxisLabelProperty( sceneProperty );
+
+    this.chartNode = new WavePlotChartNode( {
+      yAxisLabelStringProperty: yAxisLabelStringProperty,
+      xAxisLabelStringProperty: QuantumWaveInterferenceFluent.positionStringProperty,
+      x: waveRegionX + waveRegionWidth + 50,
+      y: waveRegionY + 20
+    } );
+    this.addChild( this.chartNode );
+
+    const chartConnectionProperty = new DerivedProperty(
+      [ this.chartNode.boundsProperty ],
+      bounds => bounds.leftCenter
+    );
+    const crosshairConnectionProperty = new DerivedProperty(
+      [ crosshairPositionProperty ],
+      position => position
+    );
+    const wireNormal1Property = new Vector2Property( new Vector2( -WIRE_NORMAL_DISTANCE, 0 ) );
+    const wireNormal2Property = new Vector2Property( new Vector2( WIRE_NORMAL_DISTANCE, 0 ) );
+
+    const wireNode = new WireNode( chartConnectionProperty, wireNormal1Property, crosshairConnectionProperty, wireNormal2Property, {
+      stroke: QuantumWaveInterferenceColors.graphGridLineColorProperty,
+      lineWidth: WIRE_LINE_WIDTH
+    } );
+    this.addChild( wireNode );
+    wireNode.moveToBack();
+  }
+
+  public step(): void {
+    if ( !this.visible ) {
+      return;
+    }
+
+    const scene = this.sceneProperty.value;
+    const solver = scene.waveSolver;
+    const field = solver.getAmplitudeField();
+    const gridWidth = solver.gridWidth;
+    const gridHeight = solver.gridHeight;
+    const displayMode = scene.activeWaveDisplayModeProperty.value;
+
+    const gy = clamp( Math.floor( this.lineYFractionProperty.value * gridHeight ), 0, gridHeight - 1 );
+
+    let maxAbsValue = 0;
+    const values: number[] = [];
+
+    for ( let gx = 0; gx < gridWidth; gx++ ) {
+      const idx = ( gy * gridWidth + gx ) * 2;
+      const re = field[ idx ];
+      const im = field[ idx + 1 ];
+      const value = getDisplayedWaveValue( re, im, displayMode );
+      values.push( value );
+      maxAbsValue = Math.max( maxAbsValue, Math.abs( value ) );
+    }
+
+    if ( maxAbsValue === 0 ) {
+      this.chartNode.dataPath.shape = null;
+      return;
+    }
+
+    const chartWidth = WavePlotChartNode.CHART_WIDTH;
+    const chartHeight = WavePlotChartNode.CHART_HEIGHT;
+    const shape = new Shape();
+    for ( let i = 0; i < values.length; i++ ) {
+      const x = ( i / ( values.length - 1 ) ) * chartWidth;
+      const y = chartHeight / 2 - ( values[ i ] / maxAbsValue ) * ( chartHeight / 2 - 5 );
+
+      if ( i === 0 ) {
+        shape.moveTo( x, y );
+      }
+      else {
+        shape.lineTo( x, y );
+      }
+    }
+
+    this.chartNode.dataPath.shape = shape;
+  }
+
+  public reset(): void {
+    this.lineYFractionProperty.reset();
+    this.chartNode.dataPath.shape = null;
+  }
+}
---
+++ js/common/view/WavePlotChartNode.ts
@@ -0,0 +1,124 @@
+// Copyright 2026, University of Colorado Boulder
+
+/**
+ * WavePlotChartNode is a reusable chart panel for wave measurement tools. It provides a white chart area
+ * with grid lines, a data path, axis labels, and a ShadedRectangle background. Both TimePlotNode and
+ * PositionPlotNode compose this node for their chart display.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
+import Bounds2 from '../../../../dot/js/Bounds2.js';
+import Shape from '../../../../kite/js/Shape.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import ShadedRectangle from '../../../../scenery-phet/js/ShadedRectangle.js';
+import DragListener from '../../../../scenery/js/listeners/DragListener.js';
+import Line from '../../../../scenery/js/nodes/Line.js';
+import Node, { NodeOptions } from '../../../../scenery/js/nodes/Node.js';
+import Path from '../../../../scenery/js/nodes/Path.js';
+import Rectangle from '../../../../scenery/js/nodes/Rectangle.js';
+import Text from '../../../../scenery/js/nodes/Text.js';
+
+const CHART_WIDTH = 150;
+const CHART_HEIGHT = 110;
+const LABEL_FONT = new PhetFont( 11 );
+const GRID_LINE_COLOR = '#bbbdbf';
+const AXIS_COLOR = '#808184';
+const NUM_DIVISIONS = 4;
+const PADDING = 4;
+
+type SelfOptions = {
+  yAxisLabelStringProperty: TReadOnlyProperty<string>;
+  xAxisLabelStringProperty: TReadOnlyProperty<string>;
+};
+
+type WavePlotChartNodeOptions = SelfOptions & NodeOptions;
+
+export default class WavePlotChartNode extends Node {
+
+  public readonly dataPath: Path;
+
+  public constructor( providedOptions: WavePlotChartNodeOptions ) {
+
+    const options = optionize<WavePlotChartNodeOptions, SelfOptions, NodeOptions>()( {
+      cursor: 'pointer'
+    }, providedOptions );
+
+    super();
+
+    const chartArea = new Rectangle( 0, 0, CHART_WIDTH, CHART_HEIGHT, {
+      fill: 'white',
+      stroke: AXIS_COLOR,
+      lineWidth: 1
+    } );
+
+    for ( let i = 1; i < NUM_DIVISIONS; i++ ) {
+      const y = ( i / NUM_DIVISIONS ) * CHART_HEIGHT;
+      chartArea.addChild( new Line( 0, y, CHART_WIDTH, y, {
+        stroke: GRID_LINE_COLOR,
+        lineWidth: 0.5,
+        lineDash: i === NUM_DIVISIONS / 2 ? [ 4, 4 ] : []
+      } ) );
+    }
+    for ( let i = 1; i < NUM_DIVISIONS; i++ ) {
+      const x = ( i / NUM_DIVISIONS ) * CHART_WIDTH;
+      chartArea.addChild( new Line( x, 0, x, CHART_HEIGHT, {
+        stroke: GRID_LINE_COLOR,
+        lineWidth: 0.5
+      } ) );
+    }
+
+    this.dataPath = new Path( null, {
+      stroke: 'black',
+      lineWidth: 1.5,
+      lineJoin: 'round',
+      clipArea: Shape.rectangle( 0, 0, CHART_WIDTH, CHART_HEIGHT ),
+      boundsMethod: 'none',
+      localBounds: chartArea.localBounds
+    } );
+    chartArea.addChild( this.dataPath );
+
+    const yAxisLabel = new Text( options.yAxisLabelStringProperty, {
+      font: LABEL_FONT,
+      rotation: -Math.PI / 2,
+      fill: AXIS_COLOR,
+      maxWidth: CHART_HEIGHT - 10
+    } );
+
+    const xAxisLabel = new Text( options.xAxisLabelStringProperty, {
+      font: LABEL_FONT,
+      fill: AXIS_COLOR,
+      maxWidth: CHART_WIDTH - 10
+    } );
+
+    const chartPanel = new Node( { children: [ chartArea ] } );
+
+    yAxisLabel.right = -PADDING;
+    yAxisLabel.centerY = CHART_HEIGHT / 2;
+    chartPanel.addChild( yAxisLabel );
+
+    xAxisLabel.centerX = CHART_WIDTH / 2;
+    xAxisLabel.top = CHART_HEIGHT + 3;
+    chartPanel.addChild( xAxisLabel );
+
+    // Fixed bounds: the rotated y-axis label's width is constant (≈ font height), and
+    // xAxisLabel height is constant, so these bounds are stable across locales.
+    const shadedBackground = new ShadedRectangle(
+      new Bounds2(
+        -PADDING - LABEL_FONT.numericSize - PADDING,
+        -PADDING,
+        CHART_WIDTH + PADDING,
+        CHART_HEIGHT + 3 + LABEL_FONT.numericSize + PADDING
+      )
+    );
+
+    this.children = [ shadedBackground, chartPanel ];
+    this.addInputListener( new DragListener( { translateNode: true } ) );
+    this.mutate( options );
+  }
+
+  public static readonly CHART_WIDTH = CHART_WIDTH;
+  public static readonly CHART_HEIGHT = CHART_HEIGHT;
+}
