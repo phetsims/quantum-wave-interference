26cc989b5c03cf84cd2c1a9b6cddcf2a214df798
matthew-blackman <mabl4992@colorado.edu>
2026-04-21 10:16:33 -0400

    sync-to-poly: Update left-side layout and spacing for High Intensity screen
    Mono-SHA: 9d29125a70ba494bf1d0a5aaa1fe2b1a4283bfad

--- js/common/view/createObstacleControlsRow.ts
+++ js/common/view/createObstacleControlsRow.ts
@@ -1,12 +1,11 @@
 // Copyright 2026, University of Colorado Boulder
 
 /**
- * Factory function that creates the obstacle controls row containing the obstacle combo box,
- * slit configuration combo box, and slit separation NumberControl. Used by both the
- * High Intensity and Single Particles screens.
+ * Factory function that creates the slit configuration combo box and slit separation NumberControl
+ * for the High Intensity and Single Particles screens.
  *
- * The slit configuration items differ between screens (High Intensity has detector variants,
- * Single Particles does not), so they are passed as a parameter.
+ * The controls are aligned to the wave region edges and top-aligned with the obstacle controls
+ * section.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
@@ -15,7 +14,6 @@ import type PhetioProperty from '../../../../axon/js/PhetioProperty.js';
 import { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
 import ManualConstraint from '../../../../scenery/js/layout/constraints/ManualConstraint.js';
-import HBox from '../../../../scenery/js/layout/nodes/HBox.js';
 import VBox from '../../../../scenery/js/layout/nodes/VBox.js';
 import Node from '../../../../scenery/js/nodes/Node.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
@@ -28,15 +26,8 @@ import QuantumWaveInterferenceFluent from '../../QuantumWaveInterferenceFluent.j
 import createSlitSeparationNumberControl from './createSlitSeparationNumberControl.js';
 import linkSceneVisibility from './linkSceneVisibility.js';
 
-const TITLE_FONT = new PhetFont( { size: 14, weight: 'bold' } );
-const COMBO_BOX_FONT = new PhetFont( 14 );
-const X_MARGIN = QuantumWaveInterferenceConstants.SCREEN_VIEW_X_MARGIN;
-
-type CreateObstacleControlsRowOptions = {
-  layoutBoundsBottom?: number;
-  layoutBoundsBottomOffset?: number;
-  includeObstacleSection?: boolean;
-};
+const TITLE_FONT = new PhetFont( 14 );
+const SLIT_SEPARATION_Y_OFFSET = -10;
 
 const createObstacleControlsRow = <T extends string>(
   obstacleTypeProperty: PhetioProperty<ObstacleType>,
@@ -44,36 +35,11 @@ const createObstacleControlsRow = <T extends string>(
   slitConfigItems: ComboBoxItem<T>[],
   sceneProperty: TReadOnlyProperty<BaseSceneModel>,
   scenes: BaseSceneModel[],
-  waveRegionTop: number,
+  waveRegionLeft: number,
+  obstacleAlignmentTargetNode: Node,
   listParent: Node,
-  tandem: Tandem,
-  options?: CreateObstacleControlsRowOptions
+  tandem: Tandem
 ): Node => {
-  const includeObstacleSection = options?.includeObstacleSection !== false;
-
-  const obstacleTitle = new Text( QuantumWaveInterferenceFluent.obstacleStringProperty, {
-    font: TITLE_FONT,
-    maxWidth: 120
-  } );
-
-  const obstacleComboBoxItems: ComboBoxItem<ObstacleType>[] = [
-    { value: 'none', createNode: () => new Text( QuantumWaveInterferenceFluent.noneStringProperty, { font: COMBO_BOX_FONT, maxWidth: 120 } ), tandemName: 'noneItem' },
-    { value: 'doubleSlit', createNode: () => new Text( QuantumWaveInterferenceFluent.doubleSlitStringProperty, { font: COMBO_BOX_FONT, maxWidth: 120 } ), tandemName: 'doubleSlitItem' }
-  ];
-
-  const obstacleComboBox = new ComboBox( obstacleTypeProperty, obstacleComboBoxItems, listParent, {
-    tandem: tandem.createTandem( 'obstacleComboBox' ),
-    xMargin: 10,
-    yMargin: 6,
-    listPosition: 'above'
-  } );
-
-  const obstacleSection = new VBox( {
-    spacing: 6,
-    align: 'left',
-    children: [ obstacleTitle, obstacleComboBox ]
-  } );
-
   const slitConfigTitle = new Text( QuantumWaveInterferenceFluent.slitConfigurationStringProperty, {
     font: TITLE_FONT,
     maxWidth: 150
@@ -102,42 +68,27 @@ const createObstacleControlsRow = <T extends string>(
   } );
   linkSceneVisibility( sceneProperty, scenes, slitSeparationNodes );
 
-  const slitSection = new HBox( {
-    spacing: 20,
-    align: 'bottom',
-    children: [ slitConfigSection, slitSeparationContainer ]
+  const slitControlsNode = new Node( {
+    children: [ slitConfigSection, slitSeparationContainer ],
+    excludeInvisibleChildrenFromBounds: false
   } );
 
   obstacleTypeProperty.link( obstacleType => {
-    slitSection.visible = obstacleType === 'doubleSlit';
+    slitControlsNode.visible = obstacleType === 'doubleSlit';
   } );
 
-  const bottomRowChildren = includeObstacleSection ? [ obstacleSection, slitSection ] : [ slitSection ];
+  slitConfigSection.left = 0;
+  slitConfigSection.top = 0;
 
-  const bottomRow = new HBox( {
-    spacing: 20,
-    align: 'bottom',
-    children: bottomRowChildren
+  slitSeparationContainer.right = QuantumWaveInterferenceConstants.WAVE_REGION_WIDTH;
+  slitSeparationContainer.top = SLIT_SEPARATION_Y_OFFSET;
+
+  ManualConstraint.create( listParent, [ slitControlsNode, obstacleAlignmentTargetNode ], ( slitControlsProxy, obstacleProxy ) => {
+    slitControlsProxy.x = waveRegionLeft;
+    slitControlsProxy.y = obstacleProxy.top;
   } );
-  bottomRow.left = X_MARGIN;
 
-  const Y_MARGIN = QuantumWaveInterferenceConstants.SCREEN_VIEW_Y_MARGIN;
-  if ( !includeObstacleSection ) {
-    obstacleTypeProperty.link( obstacleType => {
-      bottomRow.visible = obstacleType === 'doubleSlit';
-    } );
-  }
-  if ( options?.layoutBoundsBottom !== undefined ) {
-    const targetBottom = options.layoutBoundsBottom - Y_MARGIN + ( options.layoutBoundsBottomOffset || 0 );
-    ManualConstraint.create( listParent, [ bottomRow ], proxy => {
-      proxy.bottom = targetBottom;
-    } );
-  }
-  else {
-    bottomRow.top = waveRegionTop + QuantumWaveInterferenceConstants.WAVE_REGION_HEIGHT + 8;
-  }
-
-  return bottomRow;
+  return slitControlsNode;
 };
 
 export default createObstacleControlsRow;
