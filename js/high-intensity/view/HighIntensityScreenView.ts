7a50e6ca36686364f03a5fcd98b337c22896f42a
matthew-blackman <mabl4992@colorado.edu>
2026-04-24 10:28:14 -0400

    sync-to-poly: Refine QWI wave display defaults and source control labels - set High Intensity matter scenes to default Wave Function Display to Real Part - set Single Particles photon scenes to default Wave Display to Electric Field - set Single Particles matter scenes to default Wave Function Display to Real Part - reorder photon Wave Display options to Electric Field, then Time-averaged Intensity - reorder matter Wave Function Display options to Real Part, then Imaginary Part, then Magnitude - add configurable default matter-wave display mode support in the shared scene model - update High Intensity source slider headings so Photons show Intensity and matter scenes show Emission Rate
    Mono-SHA: 161577ec871b2418ac3eccc0ed801faabf624029

--- js/high-intensity/view/HighIntensityScreenView.ts
+++ js/high-intensity/view/HighIntensityScreenView.ts
@@ -87,7 +87,7 @@ export default class HighIntensityScreenView extends ScreenView {
 
     const sourceControlPanel = new SourceControlPanel( model.sceneProperty, model.scenes, {
       photonIntensityLabelStringProperty: QuantumWaveInterferenceFluent.intensityStringProperty,
-      particleIntensityLabelStringProperty: QuantumWaveInterferenceFluent.intensityStringProperty,
+      particleIntensityLabelStringProperty: QuantumWaveInterferenceFluent.emissionRateStringProperty,
       tandem: tandem.createTandem( 'sourceControlPanel' )
     } );
 
