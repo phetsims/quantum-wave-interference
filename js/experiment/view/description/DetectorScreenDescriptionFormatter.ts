dd48c2677307dd88737aa7f4b5d7ba03dcad50de
Sam Reid <reids@colorado.edu>
2026-05-13 22:56:54 -0600

    sync-to-poly: Factor out duplicated description, see https://github.com/phetsims/quantum-wave-interference/issues/27
    Mono-SHA: 80cc728994211baa7683152c3c03707eb221ad38

--- js/experiment/view/description/DetectorScreenDescriptionFormatter.ts
+++ js/experiment/view/description/DetectorScreenDescriptionFormatter.ts
@@ -9,6 +9,13 @@
 import QuantumWaveInterferenceFluent from '../../../QuantumWaveInterferenceFluent.js';
 import { type BandAnalysisResult, type HitStage } from './BandAnalysis.js';
 
+/**
+ * Formats the accessible detector-screen description for average-intensity mode.
+ * @param isDoubleSlit - whether the current slit configuration produces a double-slit interference pattern
+ * @param analysis - analyzed band/spacing data for the visible detector-screen region
+ * @param spatialDescription - localized spatial description of the visible pattern
+ * @returns localized accessible paragraph text
+ */
 export const formatIntensityDescription = (
   isDoubleSlit: boolean,
   analysis: BandAnalysisResult,
@@ -24,21 +31,77 @@ export const formatIntensityDescription = (
          } );
 };
 
+/**
+ * Formats the accessible detector-screen description for the live detector screen in hits mode.
+ * @param hitStage - qualitative stage of accumulated hit formation
+ * @param isDoubleSlit - whether the current slit configuration produces a double-slit interference pattern
+ * @param spatialDescription - localized spatial description of the visible pattern
+ * @returns localized accessible paragraph text
+ */
 export const formatLiveHitsDescription = (
   hitStage: HitStage,
   isDoubleSlit: boolean,
   spatialDescription: string
 ): string => {
-  return isDoubleSlit ?
-         hitStage === 'none' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsNoneStringProperty.value :
-         hitStage === 'few' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsFewStringProperty.value :
-         hitStage === 'emerging' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsEmergingStringProperty.value :
-         hitStage === 'developing' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsDeveloping.format( { spatialDescription: spatialDescription } ) :
-         hitStage === 'clear' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsClear.format( { spatialDescription: spatialDescription } ) :
-         ( () => { throw new Error( `Unrecognized hitStage: ${hitStage}` ); } )() :
-         hitStage === 'none' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsNoneStringProperty.value :
-         hitStage === 'few' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsFewStringProperty.value :
-         hitStage === 'emerging' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsSingleSlitEmergingStringProperty.value :
-         ( hitStage === 'developing' || hitStage === 'clear' ) ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsSingleSlitClear.format( { spatialDescription: spatialDescription } ) :
-         ( () => { throw new Error( `Unrecognized hitStage: ${hitStage}` ); } )();
+  if ( hitStage === 'none' ) {
+    return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsNoneStringProperty.value;
+  }
+  if ( hitStage === 'few' ) {
+    return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsFewStringProperty.value;
+  }
+
+  if ( isDoubleSlit ) {
+    return hitStage === 'emerging' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsEmergingStringProperty.value :
+           hitStage === 'developing' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsDeveloping.format( { spatialDescription: spatialDescription } ) :
+           hitStage === 'clear' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsClear.format( { spatialDescription: spatialDescription } ) :
+           ( () => { throw new Error( `Unrecognized hitStage: ${hitStage}` ); } )();
+  }
+  else {
+    return hitStage === 'emerging' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsSingleSlitEmergingStringProperty.value :
+           ( hitStage === 'developing' || hitStage === 'clear' ) ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.hitsSingleSlitClear.format( { spatialDescription: spatialDescription } ) :
+           ( () => { throw new Error( `Unrecognized hitStage: ${hitStage}` ); } )();
+  }
+};
+
+/**
+ * Formats the accessible detector-screen description for a saved snapshot in hits mode.
+ * @param hitStage - qualitative stage of accumulated hit formation
+ * @param isDoubleSlit - whether the snapshot slit configuration produces a double-slit interference pattern
+ * @param hitCount - number of hits captured in the snapshot
+ * @param spatialDescription - localized spatial description of the visible pattern
+ * @returns localized accessible paragraph text
+ */
+export const formatSnapshotHitsDescription = (
+  hitStage: HitStage,
+  isDoubleSlit: boolean,
+  hitCount: number,
+  spatialDescription: string
+): string => {
+  if ( hitStage === 'none' ) {
+    return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsNone.format( { hitCount: hitCount } );
+  }
+  if ( hitStage === 'few' ) {
+    return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsFew.format( { hitCount: hitCount } );
+  }
+
+  if ( isDoubleSlit ) {
+    return hitStage === 'emerging' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsEmerging.format( { hitCount: hitCount } ) :
+           hitStage === 'developing' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsDeveloping.format( {
+             hitCount: hitCount,
+             spatialDescription: spatialDescription
+                                     } ) :
+           hitStage === 'clear' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsClear.format( {
+                                  hitCount: hitCount,
+                                  spatialDescription: spatialDescription
+                                } ) :
+           ( () => { throw new Error( `Unrecognized hitStage: ${hitStage}` ); } )();
+  }
+  else {
+    return hitStage === 'emerging' ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsSingleSlitEmerging.format( { hitCount: hitCount } ) :
+           ( hitStage === 'developing' || hitStage === 'clear' ) ? QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsSingleSlitClear.format( {
+             hitCount: hitCount,
+             spatialDescription: spatialDescription
+                                                                 } ) :
+           ( () => { throw new Error( `Unrecognized hitStage: ${hitStage}` ); } )();
+  }
 };
--- js/experiment/view/description/SnapshotDescriber.ts
+++ js/experiment/view/description/SnapshotDescriber.ts
@@ -12,6 +12,7 @@ import type { Snapshot } from '../../../common/model/Snapshot.js';
 import QuantumWaveInterferenceFluent from '../../../QuantumWaveInterferenceFluent.js';
 import { getDetectorScreenHalfWidthForScaleIndex } from '../../model/DetectorScreenScale.js';
 import BandAnalysis from './BandAnalysis.js';
+import { formatIntensityDescription, formatSnapshotHitsDescription } from './DetectorScreenDescriptionFormatter.js';
 
 export default class SnapshotDescriber {
 
@@ -29,15 +30,7 @@ export default class SnapshotDescriber {
                                  BandAnalysis.formatSpatialArrangementDescription( analysis, isDoubleSlit, false, false ) :
                                  BandAnalysis.formatSpatialDescription( analysis, isDoubleSlit, false, false );
 
-      //REVIEW https://github.com/phetsims/quantum-wave-interference/issues/27 Same as DetectorScreenDescriber line 62
-      return isDoubleSlit ?
-             QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.intensity.format( {
-               bandCount: analysis.bandCount,
-               spatialDescription: spatialDescription
-             } ) :
-             QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.intensitySingleSlit.format( {
-               spatialDescription: spatialDescription
-             } );
+      return formatIntensityDescription( isDoubleSlit, analysis, spatialDescription );
     }
 
     const hitCount = snapshot.hits.length;
@@ -45,41 +38,6 @@ export default class SnapshotDescriber {
     const analysis = BandAnalysis.analyzeTheoreticalPatternFromSnapshot( snapshot, screenHalfWidth );
     const spatialDescription = BandAnalysis.formatSpatialDescription( analysis, isDoubleSlit, false, false );
 
-    if ( isDoubleSlit ) {
-      if ( hitStage === 'none' ) {
-        return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsNone.format( { hitCount: hitCount } );
-      }
-      if ( hitStage === 'few' ) {
-        return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsFew.format( { hitCount: hitCount } );
-      }
-      if ( hitStage === 'emerging' ) {
-        return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsEmerging.format( { hitCount: hitCount } );
-      }
-      if ( hitStage === 'developing' ) {
-        return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsDeveloping.format( {
-          hitCount: hitCount,
-          spatialDescription: spatialDescription
-        } );
-      }
-      return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsClear.format( {
-        hitCount: hitCount,
-        spatialDescription: spatialDescription
-      } );
-    }
-
-    //REVIEW https://github.com/phetsims/quantum-wave-interference/issues/27 Lots of duplication here with if statements starting at line 70.
-    if ( hitStage === 'none' ) {
-      return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsNone.format( { hitCount: hitCount } );
-    }
-    if ( hitStage === 'few' ) {
-      return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsFew.format( { hitCount: hitCount } );
-    }
-    if ( hitStage === 'emerging' ) {
-      return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsSingleSlitEmerging.format( { hitCount: hitCount } );
-    }
-    return QuantumWaveInterferenceFluent.a11y.detectorScreen.accessibleParagraph.snapshotHitsSingleSlitClear.format( {
-      hitCount: hitCount,
-      spatialDescription: spatialDescription
-    } );
+    return formatSnapshotHitsDescription( hitStage, isDoubleSlit, hitCount, spatialDescription );
   }
 }
