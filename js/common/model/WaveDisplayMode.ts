d9ee5349a8ebdde58123cfc91637a15bb9c7e1b3
Sam Reid <reids@colorado.edu>
2026-05-26 20:10:51 -0600

    sync-to-poly: Move max displayed wave value to WaveDisplayMode, see https://github.com/phetsims/quantum-wave-interference/issues/135
    Mono-SHA: 21da55fb541fa7e6c51ffa1e83acc4c79da68b6c

--- js/common/model/WaveDisplayMode.ts
+++ js/common/model/WaveDisplayMode.ts
@@ -32,3 +32,22 @@ export function getDisplayedWaveValue( re: number, im: number, displayMode: Wave
          displayMode === 'imaginaryPart' ? im :
          ( () => { throw new Error( `Unrecognized displayMode: ${displayMode}` ); } )();
 }
+
+/**
+ * Returns the maximum displayed scalar value supported by the model for a given wave display mode.
+ *
+ * The solvers use normalized wave fields:
+ * - electricField, realPart, imaginaryPart are bounded by [-1, 1]
+ * - magnitude is bounded by [0, 1]
+ * - timeAveragedIntensity is bounded by [0, 1]
+ *
+ * @param displayMode - selected representation for the displayed scalar value
+ */
+export function getMaxDisplayedWaveValue( displayMode: WaveDisplayMode ): number {
+  return displayMode === 'electricField' ? 1 :
+         displayMode === 'magnitude' ? 1 :
+         displayMode === 'realPart' ? 1 :
+         displayMode === 'imaginaryPart' ? 1 :
+         displayMode === 'timeAveragedIntensity' ? 1 :
+         ( () => { throw new Error( `Unrecognized displayMode: ${displayMode}` ); } )();
+}
