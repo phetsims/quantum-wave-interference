3fab6fa4cec5cc653f5c526393af59c5f7c26bf3
Sam Reid <reids@colorado.edu>
2026-06-10 17:20:19 -0600

    sync-to-poly: Remove unused code, see https://github.com/phetsims/quantum-wave-interference/issues/195
    Mono-SHA: 1901f6c59917b2218045cae43d0c20182579ae8b

--- js/high-intensity/view/description/QuantumWaveInterferenceAccessibleStateTemplate.ts
+++ js/high-intensity/view/description/QuantumWaveInterferenceAccessibleStateTemplate.ts
@@ -9,13 +9,9 @@
 import { type TReadOnlyProperty } from '../../../../../axon/js/TReadOnlyProperty.js';
 import { micrometersUnit } from '../../../../../scenery-phet/js/units/micrometersUnit.js';
 import { nanometersUnit } from '../../../../../scenery-phet/js/units/nanometersUnit.js';
-import type { AccessibleTemplateValue } from '../../../../../scenery/js/accessibility/pdom/ParallelDOM.js';
-import { html } from '../../../../../sherpa/lib/lit-core-3.3.1.min.js';
 import { WAVELENGTH_COLOR_ZONE_STRING_PROPERTIES } from '../../../common/view/WavelengthColorUtils.js';
 import QuantumWaveInterferenceFluent from '../../../QuantumWaveInterferenceFluent.js';
 import HighIntensityModel from '../../model/HighIntensityModel.js';
-import { type HighIntensityAccessibleViewState } from './HighIntensityAccessibleViewState.js';
-import { formatDetectorDescription, formatParticleDescription, formatSlitDescription, formatSourceBeamDescription, toFluentBoolean } from './QuantumWaveInterferenceAccessibleStateFormatters.js';
 
 export default class QuantumWaveInterferenceAccessibleStateTemplate {
 
@@ -65,49 +61,5 @@ export default class QuantumWaveInterferenceAccessibleStateTemplate {
       ...micrometersUnit.getDependentProperties(),
       ...nanometersUnit.getDependentProperties()
     ] ) );
-  }
-
-  private static createCurrentDetailsTemplate( state: HighIntensityAccessibleViewState ): AccessibleTemplateValue {
-    const overview = QuantumWaveInterferenceFluent.a11y.highIntensityState.overview.format( {
-      sourceType: state.sourceType,
-      isEmitting: toFluentBoolean( state.isEmitting ),
-      isPlaying: toFluentBoolean( state.isPlaying ),
-      detectionMode: state.detectionMode,
-      displayMode: state.displayMode
-    } );
-    const sourceStatus = QuantumWaveInterferenceFluent.a11y.highIntensityState.sourceStatus.format( {
-      isEmitting: toFluentBoolean( state.isEmitting ),
-      isPlaying: toFluentBoolean( state.isPlaying ),
-      timeSpeed: state.clockSpeedDescription
-    } );
-    const displayTools = QuantumWaveInterferenceFluent.a11y.highIntensityState.displayTools.format( {
-      displayMode: state.displayMode,
-      waveDisplayMode: state.waveDisplayMode,
-      brightness: state.screenBrightnessPercent,
-      snapshotCount: state.numberOfSnapshots,
-      tapeMeasure: toFluentBoolean( state.measurementTools.tapeMeasure.visible ),
-      stopwatch: toFluentBoolean( state.measurementTools.stopwatch.visible ),
-      timePlot: toFluentBoolean( state.measurementTools.timePlot.visible ),
-      positionPlot: toFluentBoolean( state.measurementTools.positionPlot.visible )
-    } );
-    const waveProgress = QuantumWaveInterferenceFluent.a11y.highIntensityState.waveProgress.format( {
-      waveProgressStage: state.waveProgress.stage,
-      waveDisplayMode: state.waveDisplayMode,
-      patternKind: state.patternKind
-    } );
-    const includeWaveProgress = !( state.patternKind === 'noBarrier' && state.waveProgress.stage === 'directToScreen' );
-    const sourceDescription = formatParticleDescription( state );
-    const sourceBeamDescription = formatSourceBeamDescription( state );
-    const slitDescription = formatSlitDescription( state );
-    const detectorDescription = formatDetectorDescription( state );
-
-    return html`
-      <p>${overview}</p>
-      <p>${sourceStatus}<br>${sourceBeamDescription}<br>${sourceDescription}</p>
-      <p>${slitDescription}</p>
-      ${includeWaveProgress ? html`<p>${waveProgress}</p>` : null}
-      <p>${detectorDescription}</p>
-      <p>${displayTools}</p>
-    `;
   }
 }
