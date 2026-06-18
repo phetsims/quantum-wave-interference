aa95b7e1920bfd61a5f9c35767356a887b7684c1
matthew-blackman <mabl4992@colorado.edu>
2026-06-18 14:42:34 -0400

    sync-to-poly: Fix QWI PhET-iO cross-screen naming inconsistencies, see https://github.com/phetsims/quantum-wave-interference/issues/272
    Mono-SHA: b06c47bb4ef3a6ced436f6d990fc673227e2a640

---
+++ js/experiment/view/ScreenControlsPanel.ts
@@ -0,0 +1,146 @@
+// Copyright 2026, University of Colorado Boulder
+
+/**
+ * ScreenControlsPanel contains the detection mode radio buttons ('Intensity' vs 'Hits') and a 'Screen brightness'
+ * slider. These controls affect the display on the detector screen,
+ * routed through the active scene's properties via the caller-supplied DynamicProperties.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ */
+
+import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
+import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
+import type PhetioProperty from '../../../../axon/js/PhetioProperty.js';
+import Dimension2 from '../../../../dot/js/Dimension2.js';
+import Range from '../../../../dot/js/Range.js';
+import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
+import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
+import { percentUnit } from '../../../../scenery-phet/js/units/percentUnit.js';
+import HBox from '../../../../scenery/js/layout/nodes/HBox.js';
+import VBox from '../../../../scenery/js/layout/nodes/VBox.js';
+import Text from '../../../../scenery/js/nodes/Text.js';
+import AquaRadioButtonGroup, { AquaRadioButtonGroupItem } from '../../../../sun/js/AquaRadioButtonGroup.js';
+import HSlider from '../../../../sun/js/HSlider.js';
+import Panel, { PanelOptions } from '../../../../sun/js/Panel.js';
+import { type DetectionMode } from '../../common/model/DetectionMode.js';
+import QuantumWaveInterferenceConstants from '../../common/QuantumWaveInterferenceConstants.js';
+import QuantumWaveInterferenceFluent from '../../QuantumWaveInterferenceFluent.js';
+
+const TITLE_FONT = new PhetFont( 14 );
+const LABEL_FONT = new PhetFont( 14 );
+const SETTINGS_ROW_SPACING = 40;
+
+type SelfOptions = EmptySelfOptions;
+
+type ScreenControlsPanelOptions = SelfOptions & PickRequired<PanelOptions, 'tandem'>;
+
+export default class ScreenControlsPanel extends Panel {
+
+  public constructor( detectionModeProperty: PhetioProperty<DetectionMode>,
+                      screenBrightnessProperty: PhetioProperty<number>,
+                      getDetectorScreenAccessibleContextResponse: ( mode: DetectionMode ) => string,
+                      providedOptions: ScreenControlsPanelOptions ) {
+
+    const options = optionize<ScreenControlsPanelOptions, SelfOptions, PanelOptions>()( {
+      isDisposable: false,
+      xMargin: 0,
+      yMargin: 6,
+      fill: null,
+      stroke: null
+    }, providedOptions );
+
+    // Radio buttons for Intensity vs Hits. Each button's context response describes the current detector-screen state
+    // for its mode, so switching modes is described consistently and leftover hits are never reported as "empty".
+    const radioButtonItems: AquaRadioButtonGroupItem<DetectionMode>[] = [
+      {
+        value: 'intensity',
+        createNode: () => new Text( QuantumWaveInterferenceFluent.intensityStringProperty, {
+          font: LABEL_FONT,
+          maxWidth: 130
+        } ),
+        tandemName: 'intensityRadioButton',
+        options: {
+          accessibleContextResponse: () => getDetectorScreenAccessibleContextResponse( 'intensity' )
+        }
+      },
+      {
+        value: 'hits',
+        createNode: () => new Text( QuantumWaveInterferenceFluent.hitsStringProperty, {
+          font: LABEL_FONT,
+          maxWidth: 130
+        } ),
+        tandemName: 'hitsRadioButton',
+        options: {
+          accessibleContextResponse: () => getDetectorScreenAccessibleContextResponse( 'hits' )
+        }
+      }
+    ];
+
+    const detectionModeRadioButtonGroup = new AquaRadioButtonGroup<DetectionMode>( detectionModeProperty, radioButtonItems, {
+      spacing: 8,
+      align: 'left',
+      orientation: 'vertical',
+      radioButtonOptions: {
+        radius: 7
+      },
+      touchAreaXDilation: 10,
+      mouseAreaXDilation: 10,
+      accessibleName: QuantumWaveInterferenceFluent.a11y.detectionModeRadioButtons.accessibleNameStringProperty,
+      accessibleHelpText: QuantumWaveInterferenceFluent.a11y.detectionModeRadioButtons.accessibleHelpTextStringProperty,
+      tandem: options.tandem.createTandem( 'detectionModeRadioButtonGroup' )
+    } );
+
+    // Screen brightness slider
+    const brightnessControlTandem = options.tandem.createTandem( 'brightnessControl' );
+    const brightnessLabel = new Text( QuantumWaveInterferenceFluent.screenBrightnessStringProperty, {
+      font: TITLE_FONT,
+      maxWidth: 140
+    } );
+
+    const brightnessRange = new Range( 0, QuantumWaveInterferenceConstants.SCREEN_BRIGHTNESS_MAX );
+    // NOTE: see other duplicate in quantum-wave-interference/js/common/view/BrightnessControl.ts. The brightness slider
+    // options stay local because this panel combines brightness with detection-mode controls.
+    const brightnessSlider = new HSlider( screenBrightnessProperty, brightnessRange, {
+      trackSize: new Dimension2( 130, 3 ),
+      thumbSize: new Dimension2( 13, 22 ),
+      majorTickLength: 12,
+      createAriaValueText: value => percentUnit.getAccessibleString(
+        value,
+        {
+          decimalPlaces: 0,
+          showTrailingZeros: false,
+          showIntegersAsIntegers: true
+        }
+      ),
+      accessibleName: QuantumWaveInterferenceFluent.a11y.brightnessSlider.accessibleNameStringProperty,
+      accessibleHelpText: QuantumWaveInterferenceFluent.a11y.brightnessSlider.accessibleHelpTextStringProperty,
+      tandem: brightnessControlTandem.createTandem( 'slider' ),
+      phetioVisiblePropertyInstrumented: false
+    } );
+    const brightnessControl = new VBox( {
+      spacing: 2,
+      children: [ brightnessLabel, brightnessSlider ],
+      tandem: brightnessControlTandem,
+      visiblePropertyOptions: { phetioFeatured: true }
+    } );
+
+    const content = new HBox( {
+      spacing: SETTINGS_ROW_SPACING,
+      align: 'center',
+      children: [ detectionModeRadioButtonGroup, brightnessControl ]
+    } );
+
+    const visibleProperty = new BooleanProperty( true, {
+      tandem: options.tandem.createTandem( 'visibleProperty' ),
+      phetioDocumentation: 'Controls whether the screen settings panel is allowed to be visible. ' +
+                           'The panel is automatically hidden when all of its controls are hidden.'
+    } );
+    options.visibleProperty = DerivedProperty.and( [
+      visibleProperty,
+      DerivedProperty.or( [ detectionModeRadioButtonGroup.visibleProperty, brightnessControl.visibleProperty ] )
+    ] );
+
+    super( content, options );
+  }
+}
